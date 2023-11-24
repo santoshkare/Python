e4e84926c1642f1f728c8d2b356873ab93840686
Santosh kare <142800223+santoshkare@users.noreply.github.com>
2023-11-24 21:54:12 +0530

    Add files via upload

---
+++ basic_code.py
@@ -0,0 +1,66 @@
+# Print a message to the console
+print("Hello, World!")
+
+# Add two numbers
+x = 5
+y = 10
+sum = x + y
+print("The sum of", x, "and", y, "is", sum)
+
+# Check if a number is even or odd
+num = 20
+if num % 2 == 0:
+  print(num, "is even")
+else:
+  print(num,"is odd")
+  
+# Assign an integer value to a variable
+age = 25
+print("My age is", age)
+
+# Assign a string value to a variable
+name = "John Doe"
+print("My name is", name)
+
+# Assign a boolean value to a variable
+isStudent = True
+print("I am a student:", isStudent )
+
+ # Arithmetic operators
+sum = 10 + 5
+difference = 20 - 8
+product = 3 * 4
+quotient = 16 / 2
+
+# Comparison operators
+isEqual = 10 == 10
+isGreater = 15 > 10
+isLess = 5 < 10
+
+
+# If-else statement
+age = 25
+if age >= 18:
+  print("You are an adult")
+else:
+  print("You are a minor")
+
+# elif statement
+grade = "A"
+if grade == "A":
+  print("Excellent")
+elif grade == "B":
+  print("Good")
+else:
+  print("Averageorbelow")
+  
+  
+  # For loop
+for i in range(1, 11):
+  print(i)
+
+# While loop
+count = 1
+while count <= 5:
+  print(count)
+  count+=1
