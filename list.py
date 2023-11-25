c2c225f669afa813f64df986c961e9e0d0e84b32
Santosh kare <142800223+santoshkare@users.noreply.github.com>
2023-11-25 22:15:30 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,140 @@
+##marks=[1,3,4,5,6]
+##print(type(marks))
+##if 0 in marks:
+##    print("yes")
+##else:
+##    print("no")
+##print(marks[1:])
+
+##list=[i*i for i in range(10) if i%2==0]
+##print(list)
+
+
+
+##l=[1,2,3,45,35,4,6]
+##print(l)
+###l.append(7)
+##l.sort()
+##print(l)
+
+
+
+
+##count
+##l=[1,2,3,4,5,2,34,2,3,4,53,22,45,53,2,2]
+##print(l.count(2))
+
+
+#insert
+##l=[11,45,1,2,4,6,1,1]
+##l.insert(0,899)
+##print(l)
+
+
+
+##extend()
+##l=[2,4,5,7,8,9,0]
+##m=(100,200,300)
+##l.extend(m)
+##print(l)
+
+
+
+
+
+##l=[2,4,5,7,8,9,0]
+##m=[100,200,300]
+##k=l+m
+##print(k)
+##print(l)
+
+#taking input from the user?
+# a=list (map(int,input().split()))
+# print(a)
+
+
+#
+# l=list(map(int,input().split()))
+# n=len(l)
+# i=0
+# while (i<n):
+#     j=i+1
+#     while(j<n):
+#         print(l[i],l[j])
+#         j=j+1
+#         i=i+1
+
+
+
+        #reversing list withoutusing reverse
+# l=[1,3,5,6,7,8]
+# n = len(l)
+# i = 1
+# while i<=n:
+#     print(l[-i],end=" ")
+#     i += 1
+# # print(l)
+
+
+# l=[1,3,5,6,7,8]
+# n = len(l)
+# i = -1
+# while i>= -n:
+#     print(l[i],end=" ")
+#     i -= 1
+
+# l = [1,2,3,4,5,6,7,8,9]
+# x = list()
+# for i in l[::-1]:
+#     x.append(i)
+# print(x)
+
+
+# l=[1,2,3,4,5,6,7]
+# x=list()
+# for i in l[::-1]:
+#     x.append(i)
+# print(x)
+# i=i+1   
+
+
+#target
+##l=list(map(int,input().split()))
+##n=len(l)
+##t=int(input('enter the target value'))
+##i=0
+##while(i<n-1):
+##    j=i+1
+##    while(j<n):
+##        if(l[i]+l[j]==t):
+##            print(l[i],j[j])
+##        j=j+1
+##    i=i+1        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
