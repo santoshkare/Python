671aed12dd541613e2622183ac0e090d5a79e9da
Santosh kare <142800223+santoshkare@users.noreply.github.com>
2023-11-25 23:35:40 +0530

    Add files via upload

---
+++ main_string.py
@@ -0,0 +1,6 @@
+str=(input("enter the main string"))
+sub=(input("enter the sub string"))
+if sub in str:
+   print(sub+'is found in the main string')
+else:
+   print(sub+'is not found in main string')
