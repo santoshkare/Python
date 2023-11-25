b3eb64547a533d140d53938687943a1c21043bf1
Santosh kare <142800223+santoshkare@users.noreply.github.com>
2023-11-25 22:49:29 +0530

    Add files via upload

---
+++ posi_nega.py
@@ -0,0 +1,7 @@
+x=int(input("enter the number"))
+if x==0:
+   print("the given number is zero")
+elif x>=1:
+   print("the given number is positive")
+else:
+   print("the given number is negative")
