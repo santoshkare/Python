576e4a6a0989d5c029bd98bb018295ff42366005
Santosh kare <142800223+santoshkare@users.noreply.github.com>
2023-11-26 13:22:46 +0530

    Add files via upload

---
+++ check_even.py
@@ -0,0 +1,40 @@
+#write a function to check if a number is even or not?
+"""without parameter without return value"""
+#  def num():
+#     x=int(input("enter the number"))
+#     if x%2==0:
+#        print("the number is even")
+#     else:
+#         print("the number is odd")
+# num()    
+
+
+"""without parameter without return value"""
+# def num(x):
+#     if x%2==0:
+#        print("the number is even")
+#     else:
+#         print("the number is odd")
+# x=int(input("enter the number"))
+# num(x)    
+
+"""with parameter with return value"""
+# def num(x):
+#     if x%2==0:
+#        return("the number is even")
+#     else:
+#         return("the number is odd")
+# x=int(input("enter the number"))
+# print(num(x))
+
+"""without parameter with return value"""
+def num():
+    x=int(input("enter the number"))
+    if x%2==0:
+       print("the number is even")
+    else:
+        print("the number is odd")
+print(num())  
+   
+
+
