0476f959138e1ff22e5bb6b8ee860fb36c9205de
Santosh kare <142800223+santoshkare@users.noreply.github.com>
2023-11-26 12:02:29 +0530

    Add files via upload

---
+++ area_circle.py
@@ -0,0 +1,6 @@
+#write a function to calculate area and circumference of a circle
+def area_circum():
+    r=int(input("enter the radius=="))
+    print(f"Area of circle is {3.14*r*r}")
+    print(f"circumference of circle is {2*3.14*r}")
+area_circum()
